0786863fa002f7ddef7f3183d4fa4f8221ab3733
Gavin Warnock <gbwarnock@gmail.com>
2023-02-16 13:40:31 -0700

    Finished bonus task for Lab 11 and submitting to instructors

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,11 +1,12 @@
 class AlarmClock:
     def __init__(self):
-        self.current_time = "12"
+        self.current_time = input("What time is it? ")
         self.alarm_on = False
-        self.alarm_set_to = "6"
+        self.alarm_set_to = input("When would you like to set your alarm ")
     
-    def change_time(self, new_time):
-        self.current_time = new_time
+    def change_time(self,):
+        self.current_time = self.current_time
+
     
     def alarm_toggle(self):
         if self.current_time == "6":
@@ -13,7 +14,13 @@ class AlarmClock:
         else:
             self.alarm_on = False
 
-    def set_alarm(self, new_alarm_time):
-        self.alarm_set_to = new_alarm_time
-        print(new_alarm_time)
+    def set_alarm(self):
+        self.alarm_set_to = self.alarm_set_to
+        print(f" Your alarm is set for {self.alarm_set_to}")
 
+my_alarm_clock = AlarmClock()
+
+my_alarm_clock.change_time()
+my_alarm_clock.alarm_toggle()
+my_alarm_clock.set_alarm()
+my_alarm_clock.alarm_toggle()
--- main.py
+++ main.py
@@ -4,3 +4,4 @@ person_two = Person("Gavin", 200)
 
 funds_to_transfer = person_one.send_funds(50)
 person_two.receive_funds(person_one, funds_to_transfer)
+
